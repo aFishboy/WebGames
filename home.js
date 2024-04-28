9b1cfa8b08b43c67c75448e44b77b09f907c04eb
Samuel Howard <srhoward@ucdavis.edu>
2024-04-28 00:37:30 -0700

    layout done

--- home.js
+++ home.js
@@ -1,10 +1,20 @@
 "use strict";
 
-const hamMenu = document.querySelector(".ham-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-const offScreenMenu = document.querySelector(".off-screen-menu");
+hamburger.addEventListener("click", mobileMenu);
 
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
-});
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
